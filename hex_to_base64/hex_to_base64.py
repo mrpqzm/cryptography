4f9525784945f154db5c1a5ae1d86f45dbd49cf2
mrpqzm <45233889+mrpqzm@users.noreply.github.com>
2018-11-22 08:34:37 +0800

    Update hex_to_base64.py

--- hex_to_base64/hex_to_base64.py
+++ hex_to_base64/hex_to_base64.py
@@ -1,1 +1,23 @@
-	
+import codecs
+
+
+def is_hex(s):
+    hex_digits = set("0123456789abcdef")
+    for char in s:
+        if not (char in hex_digits):
+            return False
+    return True
+
+
+hex_string = str(input("Input Your Hexadecimal Code Here: "))
+
+while is_hex(hex_string) is False:
+    hex_string = str(input("That was not Hex, Input ONLY Hexadecimal Here: "))
+
+hexdecoded = codecs.decode(hex_string, "hex")
+
+b64 = codecs.encode(codecs.decode(hex_string, "hex"), 'base64').decode()
+
+print("Your decoded Hex is: " + str(hexdecoded)[2:])
+
+print("Your encoded Base64 is: " + str(b64))
